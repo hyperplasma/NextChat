0be8f7cc4c40607b2fcdc3a471603818d5611c64
Akira37 <863781751@qq.com>
2025-08-24 00:16:12 +0800

    comment out unused ChatAction components

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import { getServerSideConfig } from "./config/server";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "KINA-Next",
+  description: "Enhance your exploration with Power!",
   appleWebApp: {
-    title: "NextChat",
+    title: "KINA-Next",
     statusBarStyle: "default",
   },
 };
